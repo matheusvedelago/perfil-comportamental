40b19e0019ae027fc64bc3cfaf9302f13f06d38b
Matheus Vedelago <matheusvedelago@gmail.com>
2026-08-04 13:06:32 +0100

    feat: adiciona primeira etapa do questionario

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -2,10 +2,12 @@ const introSection = document.getElementById("intro");
 const questionnaireSection = document.getElementById("questionnaire");
 const resultSection = document.getElementById("result");
 const startButton = document.getElementById("start-button");
+const assessmentForm = document.getElementById("assessment-form");
 
-startButton.addEventListener("click", start);
+startButton.addEventListener("click", startQuestionnaire);
+assessmentForm.addEventListener("change", handleAnswerChange);
 
-function start() {
+function startQuestionnaire() {
   introSection.hidden = true;
   questionnaireSection.hidden = false;
 }
@@ -14,3 +16,11 @@ console.log(introSection);
 console.log(questionnaireSection);
 console.log(resultSection);
 console.log(startButton);
+
+function handleAnswerChange() {
+  const selectedOption = assessmentForm.querySelector(
+    'input[name="question-1"]:checked',
+  );
+
+  console.log(selectedOption.value);
+}
