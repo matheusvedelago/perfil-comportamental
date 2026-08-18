da2a3aaf29deb2142e1c20c2c9482d893b88671c
Matheus Vedelago <matheusvedelago@gmail.com>
2026-08-18 09:50:01 +0100

    feat: calcular percentuais e perfis destacados

--- backend/src/calculations/assessmentCalculator.js
+++ backend/src/calculations/assessmentCalculator.js
@@ -1,13 +1,13 @@
 const { scoringRules } = require("../data/scoringRules");
 
-const calculateAssessment = function (answers) {
+const createDiscValues = function (answers) {
   const discValue = answers.map(function (answer) {
-    const questionId = answer.questionId;
-    const selectedOption = answer.selectedOption;
-
-    return scoringRules[questionId][selectedOption];
+    return scoringRules[answer.questionId][answer.selectedOption];
   });
+  return discValue;
+};
 
+const calculateScores = function (discValue) {
   const scores = {
     D: 0,
     I: 0,
@@ -18,10 +18,49 @@ const calculateAssessment = function (answers) {
   for (const profile of discValue) {
     scores[profile]++;
   }
+  return scores;
+};
 
-  console.log(scores);
+const calculatePercentages = function (scores) {
+  const percentages = {
+    D: 0,
+    I: 0,
+    S: 0,
+    C: 0,
+  };
 
-  return discValue;
+  let totalScores = 0;
+  for (const value in scores) {
+    totalScores += scores[value];
+  }
+
+  for (const profile in scores) {
+    percentages[profile] = Math.round((scores[profile] / totalScores) * 100);
+  }
+  return percentages;
+};
+
+const findHighlightProfiles = function (percentages) {
+  const highlightProfiles = [];
+
+  for (const profile in percentages) {
+    if (percentages[profile] >= 30) {
+      highlightProfiles.push(profile);
+    }
+  }
+
+  return highlightProfiles;
+};
+
+const calculateAssessment = function (answers) {
+  const discValue = createDiscValues(answers);
+  const scores = calculateScores(discValue);
+  const percentages = calculatePercentages(scores);
+  const highlightProfiles = findHighlightProfiles(percentages);
+  return {
+    percentages: percentages,
+    highlightProfiles: highlightProfiles,
+  };
 };
 
 module.exports = { calculateAssessment };
