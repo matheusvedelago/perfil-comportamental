21a151a0f4a097929d630e67c142fd8051961539
Matheus Vedelago <matheusvedelago@gmail.com>
2026-08-21 09:49:21 +0100

    feat: atualizar origins para deploy

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
-const allowedOrigins = ["http://localhost:5500", "http://127.0.0.1:5500"];
+const allowedOrigins = [
+  "http://localhost:5500",
+  "http://127.0.0.1:5500",
+  "https://projetos.matheusvedelago.pt",
+  "https://matheusvedelago.pt",
+];
 const assessmentRoutes = require("./src/routes/assessmentRoutes");
 const systemRoutes = require("./src/routes/systemRoutes.js");
 
